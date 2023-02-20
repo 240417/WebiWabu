562628bc46826a006e485a36569bfbf6bdd90cea
medin <medin@10.232.1.167>
2023-02-20 11:41:28 -0700

    new project Se creo empresa

--- src/mx/itson/webiwabu/entidades/Empresa.java
+++ src/mx/itson/webiwabu/entidades/Empresa.java
@@ -10,5 +10,64 @@ package mx.itson.webiwabu.entidades;
  * @author medin
  */
 public class Empresa {
-    
+    private String nombre;
+    private String teléfono;
+    private String dirección;
+    private String codigoPostal;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the teléfono
+     */
+    public String getTeléfono() {
+        return teléfono;
+    }
+
+    /**
+     * @param teléfono the teléfono to set
+     */
+    public void setTeléfono(String teléfono) {
+        this.teléfono = teléfono;
+    }
+
+    /**
+     * @return the dirección
+     */
+    public String getDirección() {
+        return dirección;
+    }
+
+    /**
+     * @param dirección the dirección to set
+     */
+    public void setDirección(String dirección) {
+        this.dirección = dirección;
+    }
+
+    /**
+     * @return the codigoPostal
+     */
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    /**
+     * @param codigoPostal the codigoPostal to set
+     */
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
 }
