1e940cc4da0d305cdd13a225e3fbe84e71654692
medin <medin@10.232.1.167>
2023-02-20 11:55:15 -0700

    new project Se creo empresa se creo cliente se creo servicio se creo costoMercado

---
+++ src/mx/itson/webiwabu/entidades/CostoMercado.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.webiwabu.entidades;
+
+/**
+ *
+ * @author medin
+ */
+public class CostoMercado {
+    private Double suministro;
+private Double distribucion;
+private Double transmicion;
+private Double cenece;
+private Double generacion;
+private Double capacidad;
+private Double scnmem;
+
+    /**
+     * @return the suministro
+     */
+    public Double getSuministro() {
+        return suministro;
+    }
+
+    /**
+     * @param suministro the suministro to set
+     */
+    public void setSuministro(Double suministro) {
+        this.suministro = suministro;
+    }
+
+    /**
+     * @return the distribucion
+     */
+    public Double getDistribucion() {
+        return distribucion;
+    }
+
+    /**
+     * @param distribucion the distribucion to set
+     */
+    public void setDistribucion(Double distribucion) {
+        this.distribucion = distribucion;
+    }
+
+    /**
+     * @return the transmicion
+     */
+    public Double getTransmicion() {
+        return transmicion;
+    }
+
+    /**
+     * @param transmicion the transmicion to set
+     */
+    public void setTransmicion(Double transmicion) {
+        this.transmicion = transmicion;
+    }
+
+    /**
+     * @return the cenece
+     */
+    public Double getCenece() {
+        return cenece;
+    }
+
+    /**
+     * @param cenece the cenece to set
+     */
+    public void setCenece(Double cenece) {
+        this.cenece = cenece;
+    }
+
+    /**
+     * @return the generacion
+     */
+    public Double getGeneracion() {
+        return generacion;
+    }
+
+    /**
+     * @param generacion the generacion to set
+     */
+    public void setGeneracion(Double generacion) {
+        this.generacion = generacion;
+    }
+
+    /**
+     * @return the capacidad
+     */
+    public Double getCapacidad() {
+        return capacidad;
+    }
+
+    /**
+     * @param capacidad the capacidad to set
+     */
+    public void setCapacidad(Double capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    /**
+     * @return the scnmem
+     */
+    public Double getScnmem() {
+        return scnmem;
+    }
+
+    /**
+     * @param scnmem the scnmem to set
+     */
+    public void setScnmem(Double scnmem) {
+        this.scnmem = scnmem;
+    }
+}
